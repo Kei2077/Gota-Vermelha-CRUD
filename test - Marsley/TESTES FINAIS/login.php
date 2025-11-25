48364e8c50671c140b9d3511a82a7110cb33875d
Marcos V. Bueno <marcosssantosbuenocontato@gmail.com>
2025-11-25 12:18:01 -0300

    final debugging before release

--- test - Marsley/TESTES FINAIS/login.php
+++ test - Marsley/TESTES FINAIS/login.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login – Gota Vermelha</title>
+  <link rel="stylesheet" href="stylelogin.css">
   <style>
     .alert {
       padding: 12px 16px;
